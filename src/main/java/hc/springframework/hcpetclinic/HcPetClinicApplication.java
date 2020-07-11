5c2e5dfdcd56cfbf236d22c929f80f1ddf62b2ce
Huseyin Cengiz <hseyin.cengiz@hotmail.com>
2020-07-11 18:01:20 +0300

    Initial Project Add

---
+++ src/main/java/hc/springframework/hcpetclinic/HcPetClinicApplication.java
@@ -0,0 +1,13 @@
+package hc.springframework.hcpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hc/springframework/hcpetclinic/HcPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package hc.springframework.hcpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
