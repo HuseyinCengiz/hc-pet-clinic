36ad42e14b84eaf2b125d70a703837c9aaaea8b4
Huseyin Cengiz <hseyin.cengiz@hotmail.com>
2020-07-12 20:23:51 +0300

    Implement Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/hc/springframework/hcpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package hc.springframework.hcpetclinic.services;
+
+import hc.springframework.hcpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
