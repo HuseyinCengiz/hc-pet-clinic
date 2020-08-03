c7ef9a16c498d10d7a342568f985d9ea3d4cbede
Huseyin Cengiz <hseyin.cengiz@hotmail.com>
2020-08-03 14:53:43 +0300

    Implementing Map based services. Closes #6.

---
+++ pet-clinic-data/src/main/java/hc/springframework/hcpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package hc.springframework.hcpetclinic.services.map;
+
+import hc.springframework.hcpetclinic.model.Vet;
+import hc.springframework.hcpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
