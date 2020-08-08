8d666b809f70efb8c35be273e9c2ff036f25f71b
Huseyin Cengiz <hseyin.cengiz@hotmail.com>
2020-08-08 23:31:40 +0300

    Added Spring Data JPA service for Visit. Closes #47.

---
+++ pet-clinic-data/src/main/java/hc/springframework/hcpetclinic/services/jpa/VisitServiceJPA.java
@@ -0,0 +1,48 @@
+package hc.springframework.hcpetclinic.services.jpa;
+
+import hc.springframework.hcpetclinic.model.Visit;
+import hc.springframework.hcpetclinic.repositories.VisitRepository;
+import hc.springframework.hcpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VisitServiceJPA implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitServiceJPA(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
