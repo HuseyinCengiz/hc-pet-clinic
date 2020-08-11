2a12d620b88b7f5a90804f1807dca1bb40696a0c
Huseyin Cengiz <hseyin.cengiz@hotmail.com>
2020-08-11 23:34:57 +0300

    Added vets json endpoint. Closes #62

--- pet-clinic-web/src/main/java/hc/springframework/hcpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/hc/springframework/hcpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package hc.springframework.hcpetclinic.controllers;
 
+import hc.springframework.hcpetclinic.model.Vet;
 import hc.springframework.hcpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -22,5 +27,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
